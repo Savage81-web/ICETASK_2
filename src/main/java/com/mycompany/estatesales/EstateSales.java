7f2813309909bc6feede188fe3b8decc9c08b078
ST10516548 <st10516548@myemeris.edu.za>
2026-08-14 08:28:34 +0000

    Commit 5: displaying sales and commision

--- src/main/java/com/mycompany/estatesales/EstateSales.java
+++ src/main/java/com/mycompany/estatesales/EstateSales.java
@@ -34,6 +34,23 @@ public class EstateSales {
         System.out.println();
 
         System.out.println("----------------------------------------------");
+        
+        // Display sales and calculate totals
+        for (int i = 0; i < sales.length; i++) {
+            System.out.printf("%-15s", agents[i]);
+
+            for (int j = 0; j < sales[i].length; j++) {
+                System.out.printf("%15.1f", sales[i][j]);
+                totals[i] += sales[i][j];
+            }
+            
+            // Calculate 2% commission
+            commissions[i] = totals[i] * 0.02;
+            System.out.println();
+        }
+        
+        
+        
 
 
     }
