a70de073855e126fd23116af365e46be49fe47d8
ST10516548 <st10516548@myemeris.edu.za>
2026-08-14 08:42:22 +0000

    Commit 13 : extending
    extending candle and getter and setter for scent

--- src/main/java/com/mycompany/estatesales/ScentedCandle.java
+++ src/main/java/com/mycompany/estatesales/ScentedCandle.java
@@ -8,6 +8,17 @@ package com.mycompany.estatesales;
  *
  * @author emeris
  */
-public class ScentedCandle {
+public class ScentedCandle extends Candle {
     
+    private String scent;
+
+    // Getter for scent
+    public String getScent() {
+        return scent;
+    }
+
+    // Setter for scent
+    public void setScent(String scent) {
+        this.scent = scent;
+    }
 }
