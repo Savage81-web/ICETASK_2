8ad1cddc003542a858e2b5f69f73478cf194bc70
ST10516548 <st10516548@myemeris.edu.za>
2026-08-14 08:19:16 +0000

    Commit 1: adding files

---
+++ src/main/java/com/mycompany/estatesales/EstateSales.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.estatesales;
+
+/**
+ *
+ * @author emeris
+ */
+public class EstateSales {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
