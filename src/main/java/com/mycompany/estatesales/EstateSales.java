80ccd1541bc9c5b7c291dd0421c1e5cae1790fae
ST10516548 <st10516548@myemeris.edu.za>
2026-08-14 08:24:54 +0000

    Commit 3: adding 2d array

--- src/main/java/com/mycompany/estatesales/EstateSales.java
+++ src/main/java/com/mycompany/estatesales/EstateSales.java
@@ -11,6 +11,19 @@ package com.mycompany.estatesales;
 public class EstateSales {
 
     public static void main(String[] args) {
-        
+        // 2D array: rows = estate agents, columns = Jan, Feb, Mar
+        double[][] sales = {
+            {800000, 1500000, 2000000},  // Joe Bloggs
+            {700000, 1200000, 1600000}   // Jane Doe
+        };
+
+        String[] agents = {"Joe Bloggs", "Jane Doe"};
+        String[] months = {"JAN", "FEB", "MAR"};
+
+        double[] totals = new double[2];
+        double[] commissions = new double[2];
+
+        System.out.println("ESTATE AGENTS SALES REPORT\n");
+
     }
 }
